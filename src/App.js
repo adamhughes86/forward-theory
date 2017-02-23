6d355527a8a3c827526db61abfbf3a5b76514ebd
Adam Hughes <adam@tokyo.uk>
2017-02-23 23:41:10 +0000

    Create Projects component and move Project within

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
-import Project from './components/Project/Project';
+import Projects from './components/Projects/Projects';
 import Technology from './components/Technology/Technology';
 import Contact from './components/Contact/Contact';
-import SuperHeading from './components/SuperHeading/SuperHeading';
+
 import './App.css';
 
 class App extends Component {
@@ -17,17 +17,7 @@ class App extends Component {
 
           <Hero/>
 
-          <section className="projects module">
-            <SuperHeading heading="Projects" />
-
-            <Project
-              title="Weather to ride"
-              imageSrc="images/weather-to-ride-01.jpg"
-              imageAlt="Weather to ride app in use"
-              text="<p>Weather to ride is a Weather app for cyclists who commute. Users are able to set weather conditions they prefer and times they commute and they receive personalised responses.</p><p>The app was built in Ionic as an internal project and is available on the ios app store.</p>"
-            />
-
-          </section>
+          <Projects/>
 
           <Technology/>
 
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import SuperHeading from '../SuperHeading/SuperHeading';
+import Project from '../Project/Project';
+import './Projects.css';
+
+class Projects extends Component {
+  render() {
+    return (
+
+      <section className="projects module">
+        <SuperHeading heading="Projects" />
+
+        <Project
+          title="Weather to ride"
+          imageSrc="images/weather-to-ride-01.jpg"
+          imageAlt="Weather to ride app in use"
+          text="<p>Weather to ride is a Weather app for cyclists who commute. Users are able to set weather conditions they prefer and times they commute and they receive personalised responses.</p><p>The app was built in Ionic as an internal project and is available on the ios app store.</p>"
+        />
+
+      </section>
+
+    );
+  }
+}
+
+export default Projects;
