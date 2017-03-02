a12e02258a86a21813e64086858ed42f00b0aec5
Adam Hughes <adam@tokyo.uk>
2017-03-02 09:09:28 +0000

    Moved GA code inline to see if it fixes issue

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import ReactGA from 'react-ga';
+ReactGA.initialize('UA-7357301-16');
 
 import Home from './components/Home/Home';
 import About from './components/About/About';
 
 import './index.scss';
-
-ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
 
 // Declarative route configuration (could also load this config lazily
 // instead, all you really need is a single root route, you don't need to
