bd331f0e7b91000dce7f3b4fc01686371cb37d01
Adam Hughes <adam@tokyo.uk>
2017-03-07 18:09:36 +0000

    Updated text for Weather to ride project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,7 +15,7 @@ class Projects extends Component {
           title="Weather to ride"
           imageSrc="images/weather-to-ride-01.jpg"
           imageAlt="Weather to ride app in use"
-          text="<p>Weather to ride is a Weather app for cyclists who commute. Users are able to set weather conditions they prefer and times they commute and they receive personalised responses.</p><p>The app was built in Ionic as an internal project and will be available in the ios app store soon.</p>"
+          text='<p>Weather to ride is an essential weather app for cyclists who commute.</p> <p>No longer will you be sat at your desk wondering "Why did I cycle in today?". Gone will be the days where you turn a corner only to be greeted by the headwind from hell.</p> <p>Set your ideal conditions and make your commute a breeze.</p><p>The app was built in Ionic as an internal project and will be available in the ios app store soon.</p>'
         />
 
       </section>
