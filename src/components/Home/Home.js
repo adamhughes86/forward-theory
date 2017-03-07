2a209b6aabaf9203dc29ba0d937dbb6bd8d421fd
Adam Hughes <adam@tokyo.uk>
2017-03-07 19:10:32 +0000

    Animated load of homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import ReactGA from 'react-ga';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import Header from '../Header/Header';
 import Hero from '../Hero/Hero';
 import Projects from '../Projects/Projects';
 import Technology from '../Technology/Technology';
 import Contact from '../Contact/Contact';
+
+import './Home.css';
 
 class Home extends Component {
 
@@ -27,14 +30,20 @@ class Home extends Component {
 
         <main className="main">
 
-          <a href="mailto:adam@forwardtheory.co.uk" className="main__cta btn-cta" onClick={this.registerClick}>Get in touch</a>
+          <ReactCSSTransitionGroup transitionName="loadButton" transitionAppear={true} transitionAppearTimeout={3800} transitionEnter={false} transitionLeave={false}>
+            <a href="mailto:adam@forwardtheory.co.uk" className="main__cta btn-cta" onClick={this.registerClick}>Get in touch</a>
+          </ReactCSSTransitionGroup>
 
-          <Hero
-            title="Crafting meaningful digital experiences"
-            text="We are a Liverpool based development studio specialising in small to medium sized websites and apps."
-          />
+          <ReactCSSTransitionGroup transitionName="hero" transitionAppear={true} transitionAppearTimeout={1600} transitionEnter={false} transitionLeave={false}>
+            <Hero
+              title="Crafting meaningful digital experiences"
+              text="We are a Liverpool based development studio specialising in small to medium sized websites and apps."
+            />
+          </ReactCSSTransitionGroup>
 
-          <Projects/>
+          <ReactCSSTransitionGroup transitionName="projects" transitionAppear={true} transitionAppearTimeout={2800} transitionEnter={false} transitionLeave={false}>
+            <Projects/>
+          </ReactCSSTransitionGroup>
 
           <Technology/>
 
