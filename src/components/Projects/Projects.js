47f54ec644d260e800dc15d9519b3192bc3e99bd
Adam Hughes <adam@tokyo.uk>
2017-03-10 13:15:06 +0000

    Fixed typo

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,7 +15,7 @@ class Projects extends Component {
           title="Weather to ride"
           imageSrc="images/weather-to-ride-promo-2.jpg"
           imageAlt="Weather to ride app in use"
-          text='<p>Weather to ride is an essential weather app for cyclists who commute.</p> <p>No longer will you be sat at your desk wondering "Why did I cycle in today?". Gone will be the days where you turn a corner only to be greeted by the headwind from hell.</p> <p>Set your ideal conditions and make your commute a breeze.</p><p>The app was built in Ionic as an internal project and will available for iPhone and iPad touch on the App Store.</p>'
+          text='<p>Weather to ride is an essential weather app for cyclists who commute.</p> <p>No longer will you be sat at your desk wondering "Why did I cycle in today?". Gone will be the days where you turn a corner only to be greeted by the headwind from hell.</p> <p>Set your ideal conditions and make your commute a breeze.</p><p>The app was built in Ionic as an internal project and is available for iPhone and iPad touch on the App Store.</p>'
           link='https://itunes.apple.com/gb/app/weather-to-ride/id1212572255'
         />
 
